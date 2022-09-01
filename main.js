d5a8e4498cc12d861c03c6eef4a06e404b1c1eea
mozaki <102328220+mozakilmao@users.noreply.github.com>
2022-09-01 16:03:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var cookies = 0;
+var clicks = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function addClick(number){
+    clicks = clicks + number;
+    document.getElementById("clicks").innerHTML = clicks;
 };
 
-var cursors = 0;
+var homelessguys = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function buyHomelessguy(){
+    var homelessguyCost = Math.floor(10 * Math.pow(1.2,homelessguys));     //works out the cost of this cursor
+    if(clicks >= homelessguyCost){                                   //checks that the player can afford the cursor
+        homelessguys = homelessguys + 1;                                   //increases number of cursors
+    	clicks = clicks - homelessguyCost;                          //removes the cookies spent
+        document.getElementById('homelessguys').innerHTML = homelessguys;  //updates the number of cursors for the user
+        document.getElementById('clicks').innerHTML = clicks;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    var nextCost = Math.floor(10 * Math.pow(1.2,homelessguys));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	addClick(cursors);
 	
 }, 1000);
